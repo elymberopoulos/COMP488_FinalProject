da88329b93efce1265cc6779bb5e49e0a05e1cee
elymberopoulos <35674633+elymberopoulos@users.noreply.github.com>
2019-04-22 18:16:19 -0500

    alarms and unlockable doors added (#20)
    * Alarm prefabs added. flashlight script worked on. level expanded more
    
    * Collision detection for key pickup added and locked door disappears if user has key boolean is true.

--- Final_Project/Assets/Scripts/PlayerMovement.cs
+++ Final_Project/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator animator;
+    public GameObject flashLight;
+    public AudioClip KeyPickupSound;
 
     float horizontonalMove = 0f;
     public float runSpeed = 40f;
@@ -15,18 +17,18 @@ public class PlayerMovement : MonoBehaviour
     bool jump = false;
     bool crouch = false;
     bool grounded = false;
+    bool flashLightOn = false;
+    bool hasYellowKey = false;
 
     void Update()
     {
         horizontonalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontonalMove));
 
-        /////////////Jumping////////////
         if (Input.GetButtonDown("Jump") && grounded)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
         }
-        /////////////Crouching////////////
 
         if (Input.GetButtonDown("Crouch"))
         {
@@ -46,23 +48,39 @@ public class PlayerMovement : MonoBehaviour
             Scene currentScene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(currentScene.name);
         }
-        /*if (collision.gameObject.tag == "MOVINGPLATFORM")
-        {
-            collision.collider.transform.SetParent(transform);
-        }*/
 
+        if(collision.gameObject.tag == "YellowKey")
+        {
+            GameObject key = GameObject.FindWithTag("YellowKey");
+            AudioSource.PlayClipAtPoint(KeyPickupSound, new Vector2(key.transform.position.x, key.transform.position.y));
+            Destroy(key);
+            hasYellowKey = true;
+        }
+
+        if(collision.gameObject.tag == "YellowForceDoor" & hasYellowKey)
+        {
+            GameObject ForceDoor = GameObject.FindWithTag("YellowForceDoor");
+            AudioSource.PlayClipAtPoint(KeyPickupSound, new Vector2(ForceDoor.transform.position.x, ForceDoor.transform.position.y));
+            Destroy(ForceDoor);
+
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         Debug.Log("Collison Exit");
         grounded = false;
-        /*if (collision.gameObject.tag == "MOVINGPLATFORM")
-        {
-            collision.collider.transform.SetParent(null);
-        }*/
-
     }
+
+    private void TurnOnFlashlight()
+    {
+        //Turn on flashlight
+        if (Input.GetButtonDown("Fire2"))
+        {
+            
+        }
+    }
+
     void FixedUpdate()
     {
         controller.Move(horizontonalMove * Time.fixedDeltaTime, crouch, jump);
